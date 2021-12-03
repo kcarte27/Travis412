1d343435cee4e3390fb51ad382a0dcb9bd944065
kaylacarter <kaylacarter@localhost>
2021-12-03 14:19:18 -0500

    Divide by zero

--- src/main/java/travis_ci_tutorial_java/SimpleCalculator.java
+++ src/main/java/travis_ci_tutorial_java/SimpleCalculator.java
@@ -16,4 +16,15 @@ public class SimpleCalculator {
 	public int divide(int a, int b) {
 		return a / b;
 	}
+	
+	public int divideByZero(int a, int b) {
+		if(b == 0) {
+			
+			throw new IllegalArgumentException("Division by zero is not allowed");
+		}
+		
+		return a / b;
+	}
+	
+	
 }
